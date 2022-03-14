ff5c1b8e55fbee4c0839146dcbef1a429c47fe40
woojin126 <woojin126@naver.com>
2022-03-14 17:08:53 +0900

    base settings

---
+++ src/test/java/com/thepirates/subject/SubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thepirates.subject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
